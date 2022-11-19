98833ed98f501099739a5d4235d8204fe939995f
ArmanKen <70683706+ArmanKen@users.noreply.github.com>
2022-11-19 21:56:10 +0200

    steps added

--- client-app/src/app/layout/FilteringOptionPickSteps.tsx
+++ client-app/src/app/layout/FilteringOptionPickSteps.tsx
@@ -7,21 +7,21 @@ export default function FilteringOptionPickSteps() {
 		<Step.Group ordered attached='top' fluid>
 			<Step completed>
 				<Step.Content>
-					<Step.Title>Shipping</Step.Title>
-					<Step.Description>Choose your shipping options</Step.Description>
+					<Step.Title></Step.Title>
+					<Step.Description></Step.Description>
 				</Step.Content>
 			</Step>
 
 			<Step completed>
 				<Step.Content>
-					<Step.Title>Billing</Step.Title>
-					<Step.Description>Enter billing information</Step.Description>
+					<Step.Title></Step.Title>
+					<Step.Description></Step.Description>
 				</Step.Content>
 			</Step>
 
 			<Step active>
 				<Step.Content>
-					<Step.Title>Confirm Order</Step.Title>
+					<Step.Title></Step.Title>
 				</Step.Content>
 			</Step>
 		</Step.Group>
